20258bb0f04bb608df99eee49c45f308f154e855
Mohammed Sufiyan <mohammedsufiyan2001@gmail.com>
2025-04-13 18:47:24 +0530

    Added theme

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,20 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/mohd_sufiyan',
+  base: '/mohd_sufiyan',
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          motion: ['framer-motion'],
+          icons: ['lucide-react'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1500, // Optional: Raise limit if needed
   },
 });
