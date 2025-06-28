c01d072919964f9724ec4b0039ad197f70aa311c
Mohammed Sufiyan <mohammedsufiyan2001@gmail.com>
2025-06-28 17:54:03 +0530

    Updates

--- assets/icons-CGGW8Fcz.js
+++ assets/icons-CGGW8Fcz.js
@@ -1,4 +1,4 @@
-import{r}from"./react-wpXbf5jk.js";/**
+import{r}from"./react-DJ1oPbzn.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
@@ -68,4 +68,4 @@ import{r}from"./react-wpXbf5jk.js";/**
  *
  * This source code is licensed under the ISC license.
  * See the LICENSE file in the root directory of this source tree.
- */const A=e("Sun",[["circle",{cx:"12",cy:"12",r:"4",key:"4exip2"}],["path",{d:"M12 2v2",key:"tus03m"}],["path",{d:"M12 20v2",key:"1lh1kg"}],["path",{d:"m4.93 4.93 1.41 1.41",key:"149t6j"}],["path",{d:"m17.66 17.66 1.41 1.41",key:"ptbguv"}],["path",{d:"M2 12h2",key:"1t8f8n"}],["path",{d:"M20 12h2",key:"1q8mjw"}],["path",{d:"m6.34 17.66-1.41 1.41",key:"1m8zz5"}],["path",{d:"m19.07 4.93-1.41 1.41",key:"1shlcs"}]]);export{g as C,M as D,b as E,f as G,L,q as M,j as P,A as S,C as a,z as b,u as c,w as d};
+ */const A=e("Sun",[["circle",{cx:"12",cy:"12",r:"4",key:"4exip2"}],["path",{d:"M12 2v2",key:"tus03m"}],["path",{d:"M12 20v2",key:"1lh1kg"}],["path",{d:"m4.93 4.93 1.41 1.41",key:"149t6j"}],["path",{d:"m17.66 17.66 1.41 1.41",key:"ptbguv"}],["path",{d:"M2 12h2",key:"1t8f8n"}],["path",{d:"M20 12h2",key:"1q8mjw"}],["path",{d:"m6.34 17.66-1.41 1.41",key:"1m8zz5"}],["path",{d:"m19.07 4.93-1.41 1.41",key:"1shlcs"}]]);export{g as C,M as D,b as E,f as G,L,q as M,j as P,A as S,C as a,w as b,z as c,u as d};
